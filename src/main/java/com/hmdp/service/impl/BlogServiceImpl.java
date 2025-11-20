bb137add1ba79605a7b60598df042f9947a0916c
BaikaiL <490051963@qq.com>
2025-11-20 11:28:06 +0800

    点赞功能实现，一人一赞，点击点赞，再点取消

--- src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,5 +1,7 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
+import com.baomidou.mybatisplus.extension.conditions.update.UpdateChainWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.Blog;
@@ -9,10 +11,15 @@ import com.hmdp.service.IBlogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.SystemConstants;
+import com.hmdp.utils.UserHolder;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.security.Key;
 import java.util.List;
+
+import static com.hmdp.utils.RedisConstants.BLOG_LIKED_KEY;
 
 /**
  * <p>
@@ -27,6 +34,9 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
 
 	@Resource
 	private IUserService userService;
+
+	@Resource
+	private StringRedisTemplate stringRedisTemplate;
 	
 	@Override
 	public Result queryBlogById(Long id) {
@@ -40,7 +50,18 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
 		
 		// 设置用户信息
 		setBlogUser(blog);
+		// 设置是否点赞
+		isBlogLiked(blog);
 		return Result.ok(blog);
+	}
+
+	private void isBlogLiked(Blog blog) {
+		// 获取用户
+		Long userId = UserHolder.getUser().getId();
+		// 判断用户是否点过赞
+		String key = BLOG_LIKED_KEY + blog.getId();
+		Boolean isLiked = stringRedisTemplate.opsForSet().isMember(key, userId.toString());
+		blog.setIsLike(isLiked);
 	}
 
 	@Override
@@ -52,8 +73,36 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
 		// 获取当前页数据
 		List<Blog> records = page.getRecords();
 		// 查询用户
-		records.forEach(this::setBlogUser);
+		records.forEach(blog -> {
+			setBlogUser(blog);
+			isBlogLiked(blog);
+		});
 		return Result.ok(records);
+	}
+
+	@Override
+	public Result likeBlog(Long id) {
+
+		// 获取用户
+		Long userId = UserHolder.getUser().getId();
+		// 判断用户是否点过赞
+		String key = BLOG_LIKED_KEY + id;
+		Boolean isLiked = stringRedisTemplate.opsForSet().isMember(key, userId.toString());
+		// 未点过，点赞数++，添加到set集合
+		if(BooleanUtil.isFalse(isLiked)){
+			boolean isSuccess = update().setSql("liked = liked + 1").eq("id", id).update();
+			if(isSuccess){
+				stringRedisTemplate.opsForSet().add(key, userId.toString());
+			}
+		}
+		else {
+			// 点过，点赞数--，移除set集合
+			boolean isSuccess = update().setSql("liked = liked - 1").eq("id", id).update();
+			if(isSuccess){
+				stringRedisTemplate.opsForSet().remove(key, userId.toString());
+			}
+		}
+		return Result.ok();
 	}
 
 	private void setBlogUser(Blog blog) {
