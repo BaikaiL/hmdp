941cb0ba8c101f3785dc7027e7396f819c9e20ae
BaikaiL <490051963@qq.com>
2025-11-21 11:26:50 +0800

    导入店铺经纬度到redis

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,9 +1,52 @@
 package com.hmdp;
 
+import com.hmdp.entity.Shop;
+import com.hmdp.service.IShopService;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
 
+	@Resource
+	private StringRedisTemplate stringRedisTemplate;
+
+	@Resource
+	private IShopService shopService;
+
+	@Test
+	public void loadShop(){
+		// 1.获取所有店铺信息
+		List<Shop> shopList = shopService.list();
+		// 2.把店铺分组，一个typeId一个组
+		Map<Long, List<Shop>> map = shopList.stream().collect(Collectors.groupingBy(Shop::getTypeId));
+		// 写入redis中
+		for (Map.Entry<Long, List<Shop>> entry : map.entrySet()) {
+			// 获取同类型店铺id
+			Long entryKey = entry.getKey();
+			String geoKey = "shop:geo:" + entryKey;
+			// 获取同类型店铺集合
+			List<Shop> list = entry.getValue();
+			List<RedisGeoCommands.GeoLocation<String>> locations = new ArrayList<>(list.size());
+			for(Shop shop : list){
+				locations.add(new RedisGeoCommands.GeoLocation<>(
+						shop.getId().toString(),
+						new Point(shop.getX(), shop.getY())
+				));
+			}
+
+			// 批量写入redis
+			stringRedisTemplate.opsForGeo().add(geoKey, locations);
+		}
+	}
 
 }
