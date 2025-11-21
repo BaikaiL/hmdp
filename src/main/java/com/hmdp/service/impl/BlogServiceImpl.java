89cbcec6c2cdc9a034d20cca32a4586778f3aefc
BaikaiL <490051963@qq.com>
2025-11-21 10:55:11 +0800

    滚动查询功能使用

--- src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -5,6 +5,7 @@ import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.ScrollResult;
 import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Blog;
 import com.hmdp.entity.Follow;
@@ -17,9 +18,11 @@ import com.hmdp.service.IUserService;
 import com.hmdp.utils.SystemConstants;
 import com.hmdp.utils.UserHolder;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
@@ -161,6 +164,54 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
 
 
 		return Result.ok(blog.getId());
+	}
+
+	@Override
+	public Result queryBlogOfFollow(Long max, Integer offset) {
+
+		// 1. 获取用户的收件箱
+		Long userId = UserHolder.getUser().getId();
+		String key = "feed:" + userId;
+
+		// 2.根据时间戳获取笔记id
+		// reverserange key min max offset count
+		Set<ZSetOperations.TypedTuple<String>> typedTuples = stringRedisTemplate.opsForZSet()
+				.reverseRangeByScoreWithScores(key, 0, max, offset, 2);
+		if(typedTuples == null || typedTuples.isEmpty()){
+			return Result.ok();
+		}
+
+		// 3.解析笔记id
+		List<Long> ids = new ArrayList<>(typedTuples.size());
+		long minTime = 0;
+		int count = 1;
+		for (ZSetOperations.TypedTuple<String> typedTuple : typedTuples) {
+			// 3.1 获取id
+			ids.add(Long.valueOf(typedTuple.getValue()));
+			// 3.2 获取时间戳
+			long time = typedTuple.getScore().longValue();
+			if(minTime == time){
+				count++;
+			}else{
+				minTime = time;
+				count = 1;
+			}
+		}
+
+		// 4.查询对应id的笔记，设置笔记的发布者和点赞数
+		String strIds = StrUtil.join(",", ids);
+		List<Blog> blogs = query().in("id", ids).last("ORDER BY FILED(id," + strIds + ")").list();
+		for (Blog blog : blogs) {
+			setBlogUser(blog);
+			isBlogLiked(blog);
+		}
+
+		// 5.封装结果并返回
+		ScrollResult scrollResult = new ScrollResult();
+		scrollResult.setList(blogs);
+		scrollResult.setOffset(count);
+		scrollResult.setMinTime(minTime);
+		return Result.ok(scrollResult);
 	}
 
 	private void setBlogUser(Blog blog) {
